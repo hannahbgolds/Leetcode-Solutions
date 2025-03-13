734426a31d27ca42d9d00f7098f75862e9acea3e
Hannah <hannahbgoldstein@gmail.com>
2025-03-13 09:20:48 -0300

    9.Palindrome solution

---
+++ Easy/Sorting/9.Palindrome/solution.c
@@ -0,0 +1,19 @@
+bool isPalindrome(int x) {
+    
+    if (x < 0) return false; 
+
+    int og = x; 
+    int rv = 0; 
+
+    while (x > 0){
+        int lastDigit = x%10; 
+        rv = rv * 10 + lastDigit;
+        x = x/10; 
+    }
+
+    if (og != rv){
+        return false; 
+    }
+
+    return og == rv;
+}
